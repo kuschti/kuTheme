70e97f1d4387b84720ec971d0fd104de09018f02
kuschti <kuschti@gmail.com>
2014-08-19 20:43:55 +0200

    add new folder structure, first gulpfile version

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    neat = require('node-neat').includePaths;
 
-gulp.task('default', function() {
-  // place code for your default task here
+var paths = {
+  scss: './src/sass/*.scss'
+};
+
+gulp.task('sass', function () {
+  return gulp.src(paths.scss)
+    .pipe(sass({
+      includePaths: ['styles'].concat(neat)
+    }))
+    .pipe(gulp.dest('./dist/css'));
 });
+
+gulp.task('default', ['sass']);
